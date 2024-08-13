86d140d67e0f3da77832dd2977ebb9289e81328b
Himanshu Suresh Chaudhari <himanshuchaudhari@Himanshus-MacBook-Air.local>
2024-08-13 15:20:59 +0530

    Init

---
+++ app/lib/constants.ts
@@ -0,0 +1,30 @@
+import { Connection } from "@solana/web3.js"
+import axios from "axios";
+import { SUPPORTED_TOKENS } from "./tokens";
+
+let LAST_UPDATED : number | null = null;
+
+let prices :{[key : string]:{
+    price : string
+}}={}
+
+const TOKEN_PRICE_REFRESH_INTERVAL = 60 * 1000; // every 60 sec 
+
+export const connection = new Connection('https://solana-mainnet.g.alchemy.com/v2/ORY6ht0auvQgGf5Oa_aHHDFo0F5sp4xW')
+
+export async function getSupportedTokens(){
+    if( !LAST_UPDATED || new Date().getTime() - LAST_UPDATED < TOKEN_PRICE_REFRESH_INTERVAL){
+        try {
+            const response = await axios.get("https://price.jup.ag/v6/price?ids=SOL,USDC,USDT");
+            prices = response.data.data;
+            LAST_UPDATED = new Date().getTime();
+        } catch(e) {
+            console.log(e);
+        }
+    }
+    return SUPPORTED_TOKENS.map(element=>({
+        ...element,
+        price : prices[element.name].price
+    }))
+}
+getSupportedTokens()
